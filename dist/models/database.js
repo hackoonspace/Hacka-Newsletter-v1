cbbe039b6f67dfa943a7932de7b01afdfa31db0c
Infinitemarcus <marcus.natrielli@outlook.com>
2022-04-06 16:38:59 -0300

    fix: Deletando pasta /dist

--- dist/models/database.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Database = void 0;
-class Database {
-}
-exports.Database = Database;
-//# sourceMappingURL=database.js.map
